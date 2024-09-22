c940a87bb8037b8b6240f2d1005e3b8723a5fe72
andy19910102 <andy19910102@gmail.com>
2024-09-22 08:48:44 +0800

    feat: add Navbar component

---
+++ components/Navbar.js
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (
+        <nav className="w-full h-12">
+            <div className="container mx-auto">
+
+            </div>
+        </nav>
+    )
+}

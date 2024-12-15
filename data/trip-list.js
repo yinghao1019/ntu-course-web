83c106ac8cfd3956800916bff155d4e92ae95f4c
andy19910102 <andy19910102@gmail.com>
2024-12-15 08:50:45 +0800

    content: add discount feature

--- data/trip-list.js
+++ data/trip-list.js
@@ -3,48 +3,64 @@ export default [
         id: "1",
         name: "Trip to Bali",
         price: 1000,
+        discountPrice: 800,
+        isDiscounted: true,
         image: "https://picsum.photos/id/317/800/600"
     },
     {
         id: "2",
         name: "Nepal Trekking",
         price: 1500,
+        discountPrice: 1500,
+        isDiscounted: false,
         image: "https://picsum.photos/id/29/800/600"
     },
     {
         id: "3",
         name: "Skiing in the Alps",
         price: 1200,
+        discountPrice: 1200,
+        isDiscounted: false,
         image: "https://picsum.photos/id/256/800/600"
     },
     {
         id: "4",
         name: "Dubai Desert Safari",
         price: 1600,
+        discountPrice: 1200,
+        isDiscounted: true,
         image: "https://picsum.photos/id/184/800/600"
     },
     {
         id: "5",
         name: "American Road Trip",
         price: 1600,
+        discountPrice: 1600,
+        isDiscounted: false,
         image: "https://picsum.photos/id/314/800/600"
     },
     {
         id: "6",
         name: "Paris City Tour",
         price: 700,
+        discountPrice: 599,
+        isDiscounted: true,
         image: "https://picsum.photos/id/318/800/600"
     },
     {
         id: "7",
         name: "Norway Snow Mountain Cable Car",
         price: 750,
+        discountPrice: 750,
+        isDiscounted: false,
         image: "https://picsum.photos/id/328/800/600"
     },
     {
         id: "8",
         name: "New York City Tour",
         price: 490,
+        discountPrice: 299,
+        isDiscounted: true,
         image: "https://picsum.photos/id/376/800/600"
     },
 ];
